d409acd4d481e5d8e3359eec916ccfd006f7f57a
jh56 <jh56@ya.ru>
2024-11-11 16:18:19 +0500

    docs

---
+++ docs/source/conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+#import sys
+#from pathlib import Path
+
+#sys.path.append(str(Path('/sphinx/ext').resolve()))
+
+
+project = 'Geo Pie'
+copyright = '2024, gis56'
+author = 'gis56'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'ru'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+#html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/source/curwells.rst
@@ -0,0 +1,33 @@
+Классы создания разреза
+=======================   
+
+Описание классов и методов
+--------------------------
+.. py:function:: lumache.get_random_ingredients(kind=None)
+
+   Return a list of random ingredients as strings.
+
+   :param kind: Optional "kind" of ingredients.
+   :type kind: list[str] or None
+   :return: The ingredients list.
+   :rtype: list[str]
+
+.. py:class:: GpAges
+.. py:method:: get(scale=1)
+
+   Метод возращает данные для создания слоя
+
+   :param int scale: Масштаб
+   :type scale: Intrger
+   :return: Кортеж
+   :rtype: tuple
+
+Исходный код::
+-------------
+
+   class GpAges:
+
+.. DANGER::
+   +------------------------------------+
+   | Внимание, внимане говорит германия |
+   +------------------------------------+
