07476eca51ffeb562b22e08b2715ccbf9512ec6e
Евгений <jh56@ya.ru>
2026-08-06 15:42:18 +0500

    plast-rst

--- docs/conf.py
+++ docs/conf.py
@@ -44,8 +44,8 @@ language = 'ru'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_show_sourcelink = True
-html_theme = 'sphinx_book_theme'
-#html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_book_theme'
+html_theme = 'sphinx_rtd_theme'
 #html_theme = 'furo'
 #html_theme = 'alabaster'
 html_static_path = ['_static']
